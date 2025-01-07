ae18bc435e85d109eedb9fc69711a090d10de303
Liza Tinku Jose <ltinkujose@ucdavis.edu>
2025-01-07 06:18:47 -0800

    login and sign up

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    user = None
+    return render_template("home.html", user=user)
 
 
