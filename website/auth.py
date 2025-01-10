c86efa2845a187fac062bdf5ef55e1e844400be4
Liza Tinku Jose <ltinkujose@ucdavis.edu>
2025-01-09 23:30:53 -0800

    updates

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -52,6 +55,12 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            flash("Account created!", category='success')
-    
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html", user=current_user)
